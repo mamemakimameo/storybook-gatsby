a73c412aa5872608940e31cb2ff04ca3b5109595
arch-mania <y.miyake@backus.co.jp>
2022-03-10 18:13:13 +0900

    Hello Worldの内容に変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
-/** @type {import('gatsby').GatsbyConfig} */
-module.exports = {
-  siteMetadata: {
-      title: ``,
-      siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
 
-  ]
+module.exports = {
+  /* Your site config here */
+  plugins: [],
 }
